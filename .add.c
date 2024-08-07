9ed523c9339ef0dc8c27b4ecf7ed190e0c8d7ac8
Arya Vijayan <aryavijayan9562@gmail.com>
2024-08-07 11:24:32 +0530

    modi3

--- .add.c
+++ .add.c
@@ -5,9 +5,11 @@
         int b=10;
         int c;
         int d;
+        int z;
         c=a+b;
         printf("%d",&c);
         d=a*b;
         printf("%d",&d);
-    
+        z=a-b;
+        printf("%d",&z);
     }
